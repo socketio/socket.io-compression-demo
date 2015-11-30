f476679848dc7fff5c6d4eaf2655f346779a0ebd
nkzawa <naoyuki.kanezawa@gmail.com>
2015-11-30 16:59:47 +0900

    don't track github

--- index.js
+++ index.js
@@ -55,7 +55,6 @@ io.on('connection', (socket) => {
 
 tw.track('socket.io');
 tw.track('javascrtipt');
-tw.track('github');
 tw.on('tweet', (tweet) => {
   debug('a new tweet: %s %s', tweet.user.name, tweet.text);
 
